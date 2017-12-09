75dcc15876eb4071d55f8fab29cdbb6a9ef1cece
BRUNA VIEIRA TEIXEIRA <41126857858@jpmorgan.sp>
2017-12-09 17:25:27 -0200

    primeiro card sem css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CardProduct from "./components/CardProduct/CardProduct";
+import teste from "./img/teste.PNG";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+// var products = [
+//     {
+//         image: {
+//             src: teste, 
+//             alt: "Notebook"
+//         },
+//         name: "teste",
+//         price: 42.99,
+//         offer: true
+//     },
+
+
+// ];
+
+var image = {
+
+  src: teste,
+  alt: "teste"
 }
 
-export default App;
+export default () => (
+
+  <div className = "App">
+    <CardProduct name="teste"  price={2323.20} image={image} />
+</div>
+
+);
