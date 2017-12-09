fa4f68b40945aa289c3dc675ba600c18e7467ac0
BRUNA VIEIRA TEIXEIRA <41126857858@jpmorgan.sp>
2017-12-09 17:31:00 -0200

    2 cards sem estrutura de repeticao

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardProduct from "./components/CardProduct/CardProduct";
 import teste from "./img/teste.PNG";
+import celular from "./img/celular.PNG"
 
 // var products = [
 //     {
@@ -22,10 +23,18 @@ var image = {
   alt: "teste"
 }
 
+var image2 = {
+
+  src: celular,
+  alt: "teste"
+}
+
 export default () => (
 
   <div className = "App">
     <CardProduct name="teste"  price={2323.20} image={image} />
+
+    <CardProduct name="teste2"  price={2323.20} image={image2} />
 </div>
 
 );
